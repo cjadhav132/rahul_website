be61313dcd09825b2ed73495a5e99613202f581c
chinmay <chinmay@ayoo.care>
2024-07-20 16:12:35 +0530

    Contact info updated

--- contact.php
+++ contact.php
@@ -1,30 +1,31 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  // $receiving_email_address = '';
+/**
+ * Requires the "PHP Email Form" library
+ * The "PHP Email Form" library is available only in the pro version of the template
+ * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
 
-  // if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-  //   include( $php_email_form );
-  // } else {
-  //   die( 'Unable to load the "PHP Email Form" Library!');
-  // }
+// Replace contact@example.com with your real receiving email address
+// $receiving_email_address = '';
 
-  // $contact = new PHP_Email_Form;
-  // $contact->ajax = true;
-  
-  // $contact->to = $receiving_email_address;
-  // $contact->from_name = $_POST['name'];
-  // $contact->from_email = $_POST['email'];
-  // $contact->subject = $_POST['subject'];
+// if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+//   include( $php_email_form );
+// } else {
+//   die( 'Unable to load the "PHP Email Form" Library!');
+// }
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+// $contact = new PHP_Email_Form;
+// $contact->ajax = true;
+
+// $contact->to = $receiving_email_address;
+// $contact->from_name = $_POST['name'];
+// $contact->from_email = $_POST['email'];
+// $contact->subject = $_POST['subject'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
@@ -33,32 +34,31 @@
   );
   */
 
-  // $contact->add_message( $_POST['name'], 'From');
-  // $contact->add_message( $_POST['email'], 'Email');
-  // $contact->add_message( $_POST['message'], 'Message', 10);
+// $contact->add_message( $_POST['name'], 'From');
+// $contact->add_message( $_POST['email'], 'Email');
+// $contact->add_message( $_POST['message'], 'Message', 10);
 
-  // echo $contact->send();
+// echo $contact->send();
 
 
 
-    // $to = "quric979797@gmail.com"; // this is your Email address
-    $to = "cjadhav132@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $move_from = $_POST['from'];
-    $move_to = $_POST['to'];
-    $phnumber = $_POST['phone'];
-    $subject = "Mail from website form submission";
-    //$subject2 = "Copy of your form submission";
-    $message = "Name = " . $name . "\n  Email =  " . $email . "\n  Wants to move from =  " . $move_from . "\n  To =  " . $move_to . "\n  Phone Number =  " . $phnumber ."\n\n They wrote additional message as follow:" . "\n" . $_POST['message'];
-    //$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+// $to = "quric979797@gmail.com"; // this is your Email address
+$to = "cjadhav132@gmail.com"; // this is your Email address
+$from = $_POST['email']; // this is the sender's Email address
+$name = $_POST['name'];
+$move_from = $_POST['from'];
+$move_to = $_POST['to'];
+$phnumber = $_POST['phone'];
+$subject = "Mail from website form submission";
+//$subject2 = "Copy of your form submission";
+$message = "Name = " . $name . "\n  Email =  " . $email . "\n  Wants to move from =  " . $move_from . "\n  To =  " . $move_to . "\n  Phone Number =  " . $phnumber . "\n\n They wrote additional message as follow:" . "\n" . $_POST['message'];
+//$message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
-    $headers = "From: noreply@yoursite.com" . "\r\n" . "CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$headers = "From: noreply@yoursite.com" . "\r\n" . "CC: somebodyelse@example.com";
+if ($email != NULL) {
+  mail($to, $subject, $txt, $headers);
 }
 //redirect
 header("Location:thankyou.html");
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-?>
+// You can also use header('Location: thank_you.php'); to redirect to another page.
+// You cannot use header and echo together. It's one or the other.
